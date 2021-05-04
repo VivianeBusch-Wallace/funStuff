21c46411faf90e5352fa1d06ecc6b82fd68c8891
viviane <v.buschwallace@yahoo.com>
2021-05-04 15:22:34 +0200

    finished weekend calculator for now

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const checkNow = document.querySelector("#checkNow");
-const userName = document.querySelector("#userName").value;
+const userName = document.querySelector("#userName");
 // testing with value here
 const date = new Date();
 
@@ -7,17 +7,20 @@ const date = new Date();
 const getDayName = () => {
   return date.toLocaleDateString("en-En", { weekday: "long" });
 };
-
+// calculation
 function weekend(e) {
   e.preventDefault();
   const result = document.querySelector(".weekend-count");
   let calc = 5 - date.getDay();
 
   if (calc > 0) {
-    result.innerHTML = `Hey ${userName}, today is ${getDayName()}, so it's already the weekend. Just joking! You still got ${calc} days left until the weekend! Almost there!`;
+    result.innerHTML = `Hey ${
+      userName.value
+    }, today is ${getDayName()}, so it's already the weekend. Just joking! You still got ${calc} days left until the weekend! Almost there!`;
   } else if (calc < 0) {
-    result.innerHTML = `Hey ${userName}, today is ${getDayName()}, so it's already the weekend! Hurray!`;
+    result.innerHTML = `Hey ${
+      userName.value
+    }, today is ${getDayName()}, so it's already the weekend! Hurray!`;
   }
 }
-
 checkNow.addEventListener("click", weekend);
