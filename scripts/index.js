425afc5617d9d24b47eecc8fcf82c67739198d96
viviane <v.buschwallace@yahoo.com>
2021-05-04 15:05:52 +0200

    finding error in js

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const checkNow = document.querySelector(".#checkNow");
+const userName = document.querySelector("#userName").value;
+// testing with value here
+const date = new Date();
+
+// getting today's weekday
+const getDayName = () => {
+  return date.toLocaleDateString("en-En", { weekday: long });
+};
+
+function weekend(e) {
+  e.preventDefault();
+  const result = document.querySelector(".weekend-count");
+  let calc = 5 - date.getDay();
+
+  if (calc > 0) {
+    result.innerHTML = `Hey ${userName}, today is ${getDayName()}, so it's already the weekend! Hurray!`;
+  } else if (calc < 0) {
+    result.innerHTML = `Hey ${userName}, today is ${getDayName()}, so it's already the weekend! Hurray!`;
+  }
+}
+
+checkNow.addEventListener("click", weekend());
