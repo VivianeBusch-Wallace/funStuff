16bcbfbdc7fea596ac4c125d114c73ad52a24775
viviane <v.buschwallace@yahoo.com>
2021-05-05 14:10:29 +0200

    number calculator half finished

--- scripts/index.js
+++ scripts/index.js
@@ -39,18 +39,39 @@ const userNumber = document.querySelector("#user-input-number");
 const calculateBtn = document.querySelector("#calculate-num-sys-btn");
 // result div
 const conversionResult = document.querySelector(".conversion-result");
-// number system chosen by user
-let numberSystem = document.querySelectorAll("#base-system option");
+// number system chosen by user to convert from
+let numberSystemFrom = document.querySelectorAll("#base-system-from");
+// number system chosen by user to convert into
+let numberSystemInto = document.querySelector("#base-system-into");
 
 function calcBin(e) {
   // prevent form from submitting all
   e.preventDefault();
-  // let result = parseInt(userNumber.value, 10).toString(2);
-  let result = parseInt(userNumber.value).toString(2);
-  // let binaryResult = userNumber.value;
+  // selected base sytem
+  let selectedOption =
+    numberSystemInto.options[numberSystemInto.selectedIndex].text;
+  console.log(userNumber.value);
+  // calculate result
+  let result;
+
+  switch (numberSystemInto.value) {
+    case "2":
+      result = parseInt(userNumber.value).toString(2);
+      break;
+    case "8":
+      result = parseInt(userNumber.value).toString(8);
+      break;
+    case "10":
+      result = parseInt(userNumber.value).toString(10);
+      break;
+    case "16":
+      result = parseInt(userNumber.value).toString(16);
+      break;
+    // default left out
+  }
 
   console.log(result);
-  conversionResult.textContent = `${userNumber.value} in ${numberSystem.innerHTML} is ${result}`;
+  conversionResult.textContent = `${userNumber.value} in ${selectedOption} is ${result}`;
 }
 
 calculateBtn.addEventListener("click", calcBin);
